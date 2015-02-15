9aa786944c4e4d20bd439f4c85867013fa6c29e4
Byrdann Fox <mobileupstate@gmail.com>
2015-02-15 16:06:30 -0500

    Updates...

---
+++ Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Byrdann Fox on 2/15/15.
+//  Copyright (c) 2015 ExcepApps, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Quiz/Quiz/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Byrdann Fox on 2/15/15.
+//  Copyright (c) 2015 ExcepApps, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
